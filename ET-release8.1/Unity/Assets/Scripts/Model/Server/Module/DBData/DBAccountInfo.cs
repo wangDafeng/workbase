485fd08cd75e3c24bedd81e7b162c61d89e2d20d
wdf <499386694@qq.com>
2024-06-20 17:31:31 +0800

    account

---
+++ ET-release8.1/Unity/Assets/Scripts/Model/Server/Module/DBData/DBAccountInfo.cs
@@ -0,0 +1,22 @@
+﻿using ET;
+
+namespace ET.Server
+{
+    public class DBAccountInfo:Entity
+    {
+
+        /// <summary>
+        /// 账号
+        /// </summary>
+        public string Account { set; get; }
+        /// <summary>
+        /// 密码
+        /// </summary>
+        public string Password { set; get; }
+        
+        /// <summary>
+        /// 第三方登录token
+        /// </summary>
+        public string LoginToken { set; get; }
+    }
+    }
