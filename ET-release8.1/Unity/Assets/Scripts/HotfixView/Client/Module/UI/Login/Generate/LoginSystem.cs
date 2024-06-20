d8ecedcbd5fc9ab71f1f59fbf9172bcc5bd1b361
wdf <499386694@qq.com>
2024-06-20 23:51:46 +0800

    denglu

--- ET-release8.1/Unity/Assets/Scripts/HotfixView/Client/Module/UI/Login/Generate/LoginSystem.cs
+++ ET-release8.1/Unity/Assets/Scripts/HotfixView/Client/Module/UI/Login/Generate/LoginSystem.cs
@@ -4,20 +4,20 @@ namespace ET.Client
 {
     public partial class LoginSystem
     {
-        // [EntitySystem]
-        // private static void Init(this Login self, GObject obj)
-        // {
-        //     self.com = obj;
-        //     self.m_account = (GTextInput)obj.asCom.GetChildAt(3);
-        //     self.m_password = (GTextInput)obj.asCom.GetChildAt(5);
-        //     self.m_Login = (GButton)obj.asCom.GetChildAt(7);
-        // }    
-        //
-        // [EntitySystem]
-        // private static void Destroy(this Login self, GObject obj)
-        // {
-        //
-        // }
+        [EntitySystem]
+        private static void Init(this Login self, GObject obj)
+        {
+            self.com = obj;
+            self.m_account = (GTextInput)obj.asCom.GetChildAt(3);
+            self.m_password = (GTextInput)obj.asCom.GetChildAt(5);
+            self.m_Login = (GButton)obj.asCom.GetChildAt(7);
+        }    
+    
+        [EntitySystem]
+        private static void Destroy(this Login self, GObject obj)
+        {
+        
+        }
     }
 }
 
--- ET-release8.1/Unity/Assets/Scripts/HotfixView/Client/Module/UI/Login/Generate/LoginSystemGenerate.cs
+++ ET-release8.1/Unity/Assets/Scripts/HotfixView/Client/Module/UI/Login/Generate/LoginSystemGenerate.cs
@@ -4,17 +4,17 @@ using FairyGUI;
 
 namespace ET.Client
 {
-    // [EntitySystemOf(typeof(Login))]
-    // [FriendOf(typeof(Login))]
-    // public static partial class LoginSystem
-    // {
-    //     [EntitySystem]
-    //     private static void Awake(this Login self, GObject obj)
-    //     {
-    //         self.com = obj;
-    //         self.m_account = (GTextInput)obj.asCom.GetChildAt(3);
-    //         self.m_password = (GTextInput)obj.asCom.GetChildAt(5);
-    //         self.m_Login = (GButton)obj.asCom.GetChildAt(7);
-    //     }
-    // }
+    [EntitySystemOf(typeof(Login))]
+    [FriendOf(typeof(Login))]
+    public static partial class LoginSystem
+    {
+        [EntitySystem]
+        private static void Awake(this Login self, GObject obj)
+        {
+            self.com = obj;
+            self.m_account = (GTextInput)obj.asCom.GetChildAt(3);
+            self.m_password = (GTextInput)obj.asCom.GetChildAt(5);
+            self.m_Login = (GButton)obj.asCom.GetChildAt(7);
+        }
+    }
 }
